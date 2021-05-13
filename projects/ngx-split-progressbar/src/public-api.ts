d0044fd707e473b0234ec6debd56ced457f2d3ea
Rohit Dandnayak <rdandnayak@gmail.com>
2021-05-13 13:17:11 +0530

    updated slot

--- projects/ngx-split-progressbar/src/public-api.ts
+++ projects/ngx-split-progressbar/src/public-api.ts
@@ -5,4 +5,4 @@
 export * from './lib/ngx-split-progressbar.service';
 export * from './lib/ngx-split-progressbar.component';
 export * from './lib/ngx-split-progressbar.module';
-export * from './lib/ngx-split-progressbar.model';
+export {ProgressBarSlot} from './lib/ngx-split-progressbar.model';
